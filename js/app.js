e323454266c4954241ce69b2062f4c95141ae2ad
Dietmar Darimont <dietmar.darimont@gmail.com>
2024-01-11 22:43:53 +0100

    fix structure

---
+++ js/app.js
@@ -0,0 +1,60 @@
+angular.module('marmont.ui', [])
+    .controller('NumpadController', ['$scope', function($scope) {
+        let vm = this;
+
+        vm.value = '';
+        vm.savedValue = '';
+        vm.titleKey = 'Mengen Eingabe';
+        vm.isNumpadVisible = false;
+        vm.isValueVisible = false;
+        vm.overrideValue = false;
+        vm.hasValueBeenAccepted = false;
+
+        vm.toggleNumpadVisibility = function() {
+            vm.isNumpadVisible = !vm.isNumpadVisible;
+            if (vm.isNumpadVisible) {
+                vm.overrideValue = true;
+            }
+        };
+
+        vm.nr = function(nr) {
+            if (vm.hasValueBeenAccepted && nr !== ',' && !vm.value.includes(',')) {
+                vm.value = '';
+                vm.hasValueBeenAccepted = false;
+            }
+
+            if (vm.value.includes(',')) {
+                let decimalPart = vm.value.split(',')[1];
+                if (decimalPart && decimalPart.length >= 3) {
+                    return;
+                }
+            }
+
+            vm.value += nr.toString();
+        };
+
+        vm.del = function() {
+            vm.value = vm.value.slice(0, -1);
+        };
+
+        vm.komma = function() {
+            if (vm.value.indexOf(',') === -1) {
+                vm.value += ',';
+            }
+        };
+
+        vm.clear = function() {
+            vm.value = '';
+        };
+
+        vm.uebernehmen = function() {
+            let cleanedValue = vm.value.replace(/^0+/, '').replace(',', '.');
+            let floatValue = parseFloat(cleanedValue);
+            if (floatValue > 0) {
+                vm.savedValue = cleanedValue;
+            }
+            vm.isValueVisible = true;
+            vm.overrideValue = true;
+            vm.hasValueBeenAccepted = true;
+        };
+    }]);
